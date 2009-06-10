9c89e2d36a198c886e09c652f528cd4543fd0efd
ze.apollo <ze.apollo@1b4b92b2-da31-11dd-8179-53c65a4d6d95>
2009-06-10 14:03:37 +0000

    replace the old oddeven js function, remove the jquery. thanks to grossopa

--- Apollo/Tuzki4.0/oddeven.js
+++ Apollo/Tuzki4.0/oddeven.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
-    $("#comments li:odd").each(function() {
-        $(this).addClass("even");
-    });
-});
+/**
+ * @author Jack Yin
+ * @version 2009-6-2 11:07
+ * @param fatherId
+ * @param eleName All the children with this tag name will be affected.
+                  <strong>Use upper cases</strong> to avoid issues.
+ * @param oddClassName
+ * @param evenClassName
+ */
+function changeEvenStyle(fatherId, eleName, oddClassName, evenClassName) {
+    var parObj = document.getElementById(fatherId);
+    var obj = parObj.firstChild;
+
+    // set default element name
+    if (eleName == null || eleName == "") {
+        eleName = "LI";
+    }
+
+    var i = 0;
+    while (obj != null) {
+        if(obj.nodeName != null && obj.nodeName == eleName) {
+            obj.className = i % 2 == 0
+                ? obj.className + " " + oddClassName
+                : obj.className + " " + evenClassName;
+            i++;
+        }
+        obj = obj.nextSibling;
+    }
+}
